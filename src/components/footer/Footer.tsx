54d67f61231737e7789287960a14b8adf86ec96e
parklego <hbd9425@gmail.com>
2024-04-04 19:57:15 +0900

    footer 로고 추가 및 정렬

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import Image from "next/image";
 import { getCurrentYear } from "@/utils/utils";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.me}>
-          © {getCurrentYear()}
-          <Link href={"https://github.com/parklego"}> parklego</Link>. All
-          rights reserved.
+        <div className={styles.sources}>
+          <div className={styles.logoWrapper}>
+            <Image src="/logo.jpeg" alt="logo" width={25} height={25} />
+            <p>youtube detox</p>
+          </div>
+          <div>
+            ©{getCurrentYear()}
+            <Link href={"https://github.com/parklego"}> parklego</Link>. All
+            rights reserved.
+          </div>
         </div>
         <div className={styles.sources}>
           <div>
-            GIF File by
+            - GIF File by
             <Link href={"https://lottiefiles.com/"}> LottieFiles</Link>
           </div>
           <div>
-            3D Designed by
+            - 3D Designed by
             <Link href={"www.freepik.com"}> Freepik</Link>
           </div>
 
           <div>
-            Data by
+            - Data by
             <Link href={"https://www.wiseapp.co.kr/"}> Wiseapp</Link>
           </div>
         </div>
