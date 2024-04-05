81622ce5c3f4d2f82b4d98c31eb244b713fb0db5
parklego <hbd9425@gmail.com>
2024-04-05 14:37:01 +0900

    로그인 페이지 프로토

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,6 +2,8 @@
 import { signIn, useSession } from "next-auth/react";
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
+import { FaGoogle } from "react-icons/fa";
+import { SiKakaotalk } from "react-icons/si";
 
 const SignInPage = () => {
   const session = useSession();
@@ -17,15 +19,26 @@ const SignInPage = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.signinTitle}>sign_in_with</div>
-      <div className={styles.buttonWrapper}>
-        <button
-          className={styles.signinButton}
-          onClick={() => signIn("google")}
-        >
-          구글로 로그인하기
-        </button>
-        <button className={styles.signinButton}>카카오로 로그인하기</button>
+      <div className={styles.signinWrapper}>
+        <div className={styles.titleWrapper}>
+          <div className={styles.signinTitle}>sign_in_with</div>
+          <div className={styles.infoTitle}>
+            간편하게 로그인하고
+            <br /> 다양한 서비스를 이용해보세요
+          </div>
+        </div>
+
+        <div className={styles.buttonWrapper}>
+          <button
+            className={`${styles.signinButton} ${styles.google}`}
+            onClick={() => signIn("google")}
+          >
+            구글로 로그인하기
+          </button>
+          <button className={`${styles.signinButton} ${styles.kakao}`}>
+            카카오로 로그인하기
+          </button>
+        </div>
       </div>
     </div>
   );
