b899bafa044f6698935cf7731c4fb5d27991ce84
parklego <hbd9425@gmail.com>
2024-04-08 01:09:32 +0900

    oAuth 적용 단계 주석 처리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+// Todo : youtube API 사용
+// 자격증명 클라이언트 측에서 확인 받기
+// 프로덕션 단계에서 해당 제출 자료 준비하기
+// https://parklego.github.io/til/think.html#next-oauth%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%98%EC%97%AC-youtube-api-%ED%98%B8%EC%B6%9C%ED%95%98%EA%B8%B0
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
