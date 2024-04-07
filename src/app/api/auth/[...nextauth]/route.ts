3dc8530327810b337891ece47698feac185d0640
parklego <hbd9425@gmail.com>
2024-04-08 01:15:24 +0900

    youtube 자격증명 단계 제거

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,32 +6,31 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-          scope:
-            "openid email profile https://www.googleapis.com/auth/youtube.readonly",
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
+      // authorization: {
+      //   params: {
+      //     scope:
+      //       "openid email profile https://www.googleapis.com/auth/youtube.readonly",
+      //     prompt: "consent",
+      //     access_type: "offline",
+      //     response_type: "code",
+      //   },
+      // },
     }),
   ],
   pages: {
     error: "/",
   },
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      session.accessToken = token.accessToken;
-
-      return session;
-    },
+    // async jwt({ token, account }) {
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //   }
+    //   return token;
+    // },
+    // async session({ session, token, user }) {
+    //   session.accessToken = token.accessToken;
+    //   return session;
+    // },
   },
 });
 
