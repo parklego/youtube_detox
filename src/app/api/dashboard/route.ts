4b6e489144058f004842dc631c46dc61b56e4140
parklego <hbd9425@gmail.com>
2024-04-10 21:19:11 +0900

    대시보드 시간가져오기

---
+++ src/app/api/dashboard/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const POST = async (request: Request, response: Response) => {
+  try {
+    const data = await request.json();
+    const { id } = data;
+
+    const result = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    const returnData = {
+      initialTime: result?.initialTime,
+      detoxTime: result?.detoxTime,
+    };
+    return NextResponse.json(returnData);
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  }
+};
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,28 +3,67 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "sonner";
 
 const Dashboard = () => {
   const session = useSession();
   const router = useRouter();
 
-  console.log(session);
-  if (session.status === "loading") {
+  const [record, setRecord] = useState({
+    initialTime: "",
+    detoxTime: "",
+  });
+
+  const { status, data } = session;
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      if (data) {
+        const response = await axios.post("/api/dashboard", {
+          id: data.user.id,
+        });
+
+        if (response.status === 200) {
+          setRecord(response.data);
+        } else {
+          toast.error("기록된 정보를 불러오는데 실패하였습니다.");
+        }
+      }
+    };
+
+    dataFetch();
+  }, [data]);
+  // console.log(record);
+  if (status === "loading") {
     return <div className={styles.container}>loading..</div>;
   }
 
-  if (session.status !== "authenticated") {
+  if (status === "unauthenticated") {
     router.push("/signin");
   }
 
-  if (session.status === "authenticated") {
-    return (
-      <div className={styles.dashboard}>
-        <p>{`반가워요. ${session.data.user?.name}님`}</p>
-        <p>오늘도 소중한 시간을 만들어봐요.</p>
+  return (
+    <div className={styles.dashboard}>
+      <p>{`반가워요. ${data?.user?.name}님.`}</p>
+      <p>오늘도 소중한 시간을 만들어봐요.</p>
+
+      <div className={styles.recordWrapper}>
+        <div className={styles.timeWrapper}>
+          <p> 디톡스 전 기록된 유튜브 시청 시간</p>
+          <p>
+            {record?.initialTime ? record.initialTime : "아직 입력 전입니다."}
+          </p>
+        </div>
+
+        <div className={styles.timeWrapper}>
+          <p>디톡스 후 기록된 유튜브 시청 시간</p>
+          <p>{record?.detoxTime ? record.detoxTime : "아직 입력 전입니다."}</p>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Dashboard;
