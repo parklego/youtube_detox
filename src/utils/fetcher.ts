0d49d13b97b4017c46adbb4b883da717ab9fe405
parklego <hbd9425@gmail.com>
2024-04-11 17:37:48 +0900

    swr제거

--- src/utils/fetcher.ts
+++
@@ -1,9 +0,0 @@
-export const fetcher = async <T>(
-  ...args: Parameters<typeof fetch>
-): Promise<T> => {
-  const response = await fetch(...args);
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return response.json();
-};
