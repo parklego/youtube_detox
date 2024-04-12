4001629168b48c0848e5d3ccb49d07f2a04bda37
parklego <hbd9425@gmail.com>
2024-04-13 04:06:00 +0900

    category video mock data

--- src/app/resources/mock.ts
+++ src/app/resources/mock.ts
@@ -142,3 +142,349 @@ export const youtubeMock = [
     },
   },
 ];
+
+export const categoryMock = [
+  {
+    id: "5u-8JQcHmq1xtG0AYCY3d",
+    name: "예능",
+    channel: [
+      {
+        id: {
+          kind: "youtube#channel",
+          channelId: "UCsJ6RuBiTVWRX156FVbeaGg",
+        },
+        etag: "ukekNklh0y1eYPdeoJmKXs0xiYA",
+        kind: "youtube#searchResult",
+        snippet: {
+          title: "슈카월드",
+          channelId: "UCsJ6RuBiTVWRX156FVbeaGg",
+          thumbnails: {
+            high: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_l3IU6711Ae_FuslHgP5zdxkuvLyoGx_8sESS9je5iO2C0=s800-c-k-c0xffffffff-no-rj-mo",
+            },
+            medium: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_l3IU6711Ae_FuslHgP5zdxkuvLyoGx_8sESS9je5iO2C0=s240-c-k-c0xffffffff-no-rj-mo",
+            },
+            default: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_l3IU6711Ae_FuslHgP5zdxkuvLyoGx_8sESS9je5iO2C0=s88-c-k-c0xffffffff-no-rj-mo",
+            },
+          },
+          description:
+            "경제, 금융을 기반으로 달리는 방송! 정치는 아주. 전혀. 대단히. 모릅니다. 누군가를 비판하거나 비난하지 않도록 노력하겠습니다.",
+          publishTime: "2015-04-25T03:34:03Z",
+          publishedAt: "2015-04-25T03:34:03Z",
+          channelTitle: "슈카월드",
+          liveBroadcastContent: "upcoming",
+        },
+      },
+      {
+        id: {
+          kind: "youtube#channel",
+          channelId: "UCw2zOqd6hXsny79qDki_Z4w",
+        },
+        etag: "eEvCEmpceiOJ8Tax9qWsaBfxXGc",
+        kind: "youtube#searchResult",
+        snippet: {
+          title: "삐라",
+          channelId: "UCw2zOqd6hXsny79qDki_Z4w",
+          thumbnails: {
+            high: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_kcLue1B9TFgJs4e2yq-HSc3gvaARCEzYogt0FsZ9mgYWc=s800-c-k-c0xffffffff-no-rj-mo",
+            },
+            medium: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_kcLue1B9TFgJs4e2yq-HSc3gvaARCEzYogt0FsZ9mgYWc=s240-c-k-c0xffffffff-no-rj-mo",
+            },
+            default: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_kcLue1B9TFgJs4e2yq-HSc3gvaARCEzYogt0FsZ9mgYWc=s88-c-k-c0xffffffff-no-rj-mo",
+            },
+          },
+          description:
+            "오버워치 및 종합게임을 주로하는 삐라 유튜브입니다!! 생방송은 오후 10시에 합니다!! ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ ...",
+          publishTime: "2016-10-31T08:04:23Z",
+          publishedAt: "2016-10-31T08:04:23Z",
+          channelTitle: "삐라",
+          liveBroadcastContent: "live",
+        },
+      },
+    ],
+  },
+  {
+    id: "ifc8pQrj5iFYIEbxi2U96",
+    name: "축구",
+    channel: [
+      {
+        id: {
+          kind: "youtube#channel",
+          channelId: "UCsJ6RuBiTVWRX156FVbeaGg",
+        },
+        etag: "ukekNklh0y1eYPdeoJmKXs0xiYA",
+        kind: "youtube#searchResult",
+        snippet: {
+          title: "슈카월드",
+          channelId: "UCsJ6RuBiTVWRX156FVbeaGg",
+          thumbnails: {
+            high: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_l3IU6711Ae_FuslHgP5zdxkuvLyoGx_8sESS9je5iO2C0=s800-c-k-c0xffffffff-no-rj-mo",
+            },
+            medium: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_l3IU6711Ae_FuslHgP5zdxkuvLyoGx_8sESS9je5iO2C0=s240-c-k-c0xffffffff-no-rj-mo",
+            },
+            default: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_l3IU6711Ae_FuslHgP5zdxkuvLyoGx_8sESS9je5iO2C0=s88-c-k-c0xffffffff-no-rj-mo",
+            },
+          },
+          description:
+            "경제, 금융을 기반으로 달리는 방송! 정치는 아주. 전혀. 대단히. 모릅니다. 누군가를 비판하거나 비난하지 않도록 노력하겠습니다.",
+          publishTime: "2015-04-25T03:34:03Z",
+          publishedAt: "2015-04-25T03:34:03Z",
+          channelTitle: "슈카월드",
+          liveBroadcastContent: "upcoming",
+        },
+      },
+      {
+        id: {
+          kind: "youtube#channel",
+          channelId: "UCw2zOqd6hXsny79qDki_Z4w",
+        },
+        etag: "eEvCEmpceiOJ8Tax9qWsaBfxXGc",
+        kind: "youtube#searchResult",
+        snippet: {
+          title: "삐라",
+          channelId: "UCw2zOqd6hXsny79qDki_Z4w",
+          thumbnails: {
+            high: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_kcLue1B9TFgJs4e2yq-HSc3gvaARCEzYogt0FsZ9mgYWc=s800-c-k-c0xffffffff-no-rj-mo",
+            },
+            medium: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_kcLue1B9TFgJs4e2yq-HSc3gvaARCEzYogt0FsZ9mgYWc=s240-c-k-c0xffffffff-no-rj-mo",
+            },
+            default: {
+              url: "https://yt3.ggpht.com/ytc/AIdro_kcLue1B9TFgJs4e2yq-HSc3gvaARCEzYogt0FsZ9mgYWc=s88-c-k-c0xffffffff-no-rj-mo",
+            },
+          },
+          description:
+            "오버워치 및 종합게임을 주로하는 삐라 유튜브입니다!! 생방송은 오후 10시에 합니다!! ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ ...",
+          publishTime: "2016-10-31T08:04:23Z",
+          publishedAt: "2016-10-31T08:04:23Z",
+          channelTitle: "삐라",
+          liveBroadcastContent: "live",
+        },
+      },
+    ],
+  },
+  {
+    id: "Ugp06-alxfg-S3GkLA9B8",
+    name: "경제",
+    channel: [],
+  },
+];
+
+export const resultVidoe = [
+  {
+    id: "5u-8JQcHmq1xtG0AYCY3d",
+    name: "예능",
+    video: [
+      {
+        kind: "youtube#searchListResponse",
+        etag: "25Vv2jaPe2fKv8m8YDwdxUvXu-w",
+        nextPageToken: "CAEQAA",
+        regionCode: "KR",
+        pageInfo: {
+          totalResults: 279628,
+          resultsPerPage: 1,
+        },
+        items: [
+          {
+            kind: "youtube#searchResult",
+            etag: "yNuwalUwDd5CJAasebXKIrszcmw",
+            id: {
+              kind: "youtube#video",
+              videoId: "pwmt-CQ10Mk",
+            },
+            snippet: {
+              publishedAt: "2022-09-09T13:17:42Z",
+              channelId: "UCsJ6RuBiTVWRX156FVbeaGg",
+              title: "전갈 집게는 다리가 아니다?",
+              description:
+                "어렵고 딱딱한 경제,시사,금융 이야기를 쉽고 유쾌하게 풀어내는 경제/시사/이슈/잡썰 토크방송입니다. #스콜피온 #스카너.",
+              thumbnails: {
+                default: {
+                  url: "https://i.ytimg.com/vi/pwmt-CQ10Mk/default.jpg",
+                  width: 120,
+                  height: 90,
+                },
+                medium: {
+                  url: "https://i.ytimg.com/vi/pwmt-CQ10Mk/mqdefault.jpg",
+                  width: 320,
+                  height: 180,
+                },
+                high: {
+                  url: "https://i.ytimg.com/vi/pwmt-CQ10Mk/hqdefault.jpg",
+                  width: 480,
+                  height: 360,
+                },
+              },
+              channelTitle: "슈카월드",
+              liveBroadcastContent: "none",
+              publishTime: "2022-09-09T13:17:42Z",
+            },
+          },
+        ],
+      },
+      {
+        kind: "youtube#searchListResponse",
+        etag: "Dwdni9zAe7GdZO29nfYtU9qmW9U",
+        nextPageToken: "CAEQAA",
+        regionCode: "KR",
+        pageInfo: {
+          totalResults: 23520,
+          resultsPerPage: 1,
+        },
+        items: [
+          {
+            kind: "youtube#searchResult",
+            etag: "zIQImdz2V4W9rkesuJM6DruFHJo",
+            id: {
+              kind: "youtube#video",
+              videoId: "JkIo50BkcMc",
+            },
+            snippet: {
+              publishedAt: "2021-08-13T12:14:16Z",
+              channelId: "UCw2zOqd6hXsny79qDki_Z4w",
+              title:
+                "IQ 999 솜브라 고인물이 윈스턴을 농락하는 법 ㅋㅋㅋ #Shorts",
+              description:
+                "(20210813) 삐라의 오버워치 및 종합게임 유튜브채널입니다!! ♥삐라의 생방송(트위치) : https://www.twitch.tv/bbira ♥생방송 공지: ...",
+              thumbnails: {
+                default: {
+                  url: "https://i.ytimg.com/vi/JkIo50BkcMc/default.jpg",
+                  width: 120,
+                  height: 90,
+                },
+                medium: {
+                  url: "https://i.ytimg.com/vi/JkIo50BkcMc/mqdefault.jpg",
+                  width: 320,
+                  height: 180,
+                },
+                high: {
+                  url: "https://i.ytimg.com/vi/JkIo50BkcMc/hqdefault.jpg",
+                  width: 480,
+                  height: 360,
+                },
+              },
+              channelTitle: "삐라",
+              liveBroadcastContent: "none",
+              publishTime: "2021-08-13T12:14:16Z",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "ifc8pQrj5iFYIEbxi2U96",
+    name: "축구",
+    video: [
+      {
+        kind: "youtube#searchListResponse",
+        etag: "pPqRClK0NcBkHrzYz3MEDmczuFM",
+        nextPageToken: "CAEQAA",
+        regionCode: "KR",
+        pageInfo: {
+          totalResults: 279374,
+          resultsPerPage: 1,
+        },
+        items: [
+          {
+            kind: "youtube#searchResult",
+            etag: "yNuwalUwDd5CJAasebXKIrszcmw",
+            id: {
+              kind: "youtube#video",
+              videoId: "pwmt-CQ10Mk",
+            },
+            snippet: {
+              publishedAt: "2022-09-09T13:17:42Z",
+              channelId: "UCsJ6RuBiTVWRX156FVbeaGg",
+              title: "전갈 집게는 다리가 아니다?",
+              description:
+                "어렵고 딱딱한 경제,시사,금융 이야기를 쉽고 유쾌하게 풀어내는 경제/시사/이슈/잡썰 토크방송입니다. #스콜피온 #스카너.",
+              thumbnails: {
+                default: {
+                  url: "https://i.ytimg.com/vi/pwmt-CQ10Mk/default.jpg",
+                  width: 120,
+                  height: 90,
+                },
+                medium: {
+                  url: "https://i.ytimg.com/vi/pwmt-CQ10Mk/mqdefault.jpg",
+                  width: 320,
+                  height: 180,
+                },
+                high: {
+                  url: "https://i.ytimg.com/vi/pwmt-CQ10Mk/hqdefault.jpg",
+                  width: 480,
+                  height: 360,
+                },
+              },
+              channelTitle: "슈카월드",
+              liveBroadcastContent: "none",
+              publishTime: "2022-09-09T13:17:42Z",
+            },
+          },
+        ],
+      },
+      {
+        kind: "youtube#searchListResponse",
+        etag: "8PY__TfmoKjCDZI-PmWQ2gIaSPY",
+        nextPageToken: "CAEQAA",
+        regionCode: "KR",
+        pageInfo: {
+          totalResults: 19676,
+          resultsPerPage: 1,
+        },
+        items: [
+          {
+            kind: "youtube#searchResult",
+            etag: "zIQImdz2V4W9rkesuJM6DruFHJo",
+            id: {
+              kind: "youtube#video",
+              videoId: "JkIo50BkcMc",
+            },
+            snippet: {
+              publishedAt: "2021-08-13T12:14:16Z",
+              channelId: "UCw2zOqd6hXsny79qDki_Z4w",
+              title:
+                "IQ 999 솜브라 고인물이 윈스턴을 농락하는 법 ㅋㅋㅋ #Shorts",
+              description:
+                "(20210813) 삐라의 오버워치 및 종합게임 유튜브채널입니다!! ♥삐라의 생방송(트위치) : https://www.twitch.tv/bbira ♥생방송 공지: ...",
+              thumbnails: {
+                default: {
+                  url: "https://i.ytimg.com/vi/JkIo50BkcMc/default.jpg",
+                  width: 120,
+                  height: 90,
+                },
+                medium: {
+                  url: "https://i.ytimg.com/vi/JkIo50BkcMc/mqdefault.jpg",
+                  width: 320,
+                  height: 180,
+                },
+                high: {
+                  url: "https://i.ytimg.com/vi/JkIo50BkcMc/hqdefault.jpg",
+                  width: 480,
+                  height: 360,
+                },
+              },
+              channelTitle: "삐라",
+              liveBroadcastContent: "none",
+              publishTime: "2021-08-13T12:14:16Z",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "Ugp06-alxfg-S3GkLA9B8",
+    name: "경제",
+    video: [],
+  },
+];
